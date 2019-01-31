ce9ab8b35675ec0a3c97e5222abbaf21abfe0ec1
Madi <madelyn.e.helton@gmail.com>
2019-01-30 18:11:08 -0600

    01302019

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let mysql = require("mysql");
 let express = require("express");
 let path = require("path");
+let app = express();
 
 let PORT = process.env.PORT || 3000;
 
@@ -9,16 +10,28 @@ let PORT = process.env.PORT || 3000;
 // app.use(express.json());
 
 
+
+app.get("/",function(request, response){
+  response.send("<h1>Works!</h1>");
+  
+});
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
+
 // demo from class 01/07/19
-let http = require("http");
+// let http = require("http");
 
-let handleRequest = function(request, response) {
-  response.end("It worked. Path hit: " + request.url);
-};
+// let handleRequest = function(request, response) {
+//   response.end("It worked. Path hit: " + request.url);
+// };
 
-let server = http.createServer(handleRequest);
+// let server = http.createServer(handleRequest);
 
-server.listen(PORT, function(){
-  console.log("Server is listening on: http://localhost:%s", PORT);
-})
+// server.listen(PORT, function(){
+//   console.log("Server is listening on: http://localhost:%s", PORT);
+// })
 // localhost = 127.0.0.1;
